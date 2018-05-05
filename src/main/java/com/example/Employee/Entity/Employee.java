49e425899e7bac142c62b03b7074b4a18d5e2f10
summer2023 <happyxia1023@163.com>
2018-05-05 14:12:40 +0800

    create entity employee

--- src/main/java/com/example/Employee/Entity/Employee.java
+++ src/main/java/com/example/Employee/Entity/Employee.java
@@ -1,8 +1,48 @@
 package com.example.Employee.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name="t_employee")
 public class Employee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name=name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge() {
+        this.age=age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender() {
+        this.gender=gender;
+    }
+
+
 }
