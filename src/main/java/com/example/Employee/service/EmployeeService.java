ec460e7d50f409a2098deaf349136d9681d1a3c8
summer2023 <happyxia1023@163.com>
2018-05-05 14:24:55 +0800

    create service Employeeservice

--- src/main/java/com/example/Employee/service/EmployeeService.java
+++ src/main/java/com/example/Employee/service/EmployeeService.java
@@ -1,4 +1,21 @@
 package com.example.Employee.service;
 
+import com.example.Employee.Entity.Employee;
+import com.example.Employee.dao.EmployeeRepositoty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepositoty employeeRepositoty;
+    public Employee findEmployeeByName(String name){
+        Employee employee=null;
+        try {
+            employee = employeeRepositoty.findByEmployeeName(name);
+        } catch (Exception e) {
+
+        }
+        return employee;
+    }
 }
